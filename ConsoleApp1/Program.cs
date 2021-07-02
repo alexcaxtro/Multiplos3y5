08178ebde3807a48ae127b7317aa2bb9674aa85c
OSCAR ALEXANDER CASTRO SALAS <oscar.castro21@inacapmail.cl>
2021-07-02 17:00:40 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Hola" + i);
+                }
+                if (i%5==0)
+                {
+                    Console.WriteLine("Mundo" + i);
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("HolaMundo" + i);
+                }
+
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
